dbb9afb8636a3f762a1bc704115e8b9902e52405
Roberth González <reliutg@gmail.com>
2024-05-04 17:17:22 +0200

    Refactor ChatMessage component to use markdown-it library

---
+++ components/ChatMessage.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import MarkdownIt from "markdown-it";
+import MarkdownItAbbr from "markdown-it-abbr";
+import MarkdownItAnchor from "markdown-it-anchor";
+import MarkdownItFootnote from "markdown-it-footnote";
+import MarkdownItHighlightjs from "markdown-it-highlightjs";
+import MarkdownItSub from "markdown-it-sub";
+import MarkdownItSup from "markdown-it-sup";
+import MarkdownItTasklists from "markdown-it-task-lists";
+import MarkdownItTOC from "markdown-it-toc-done-right";
+
+const markdown = new MarkdownIt()
+  .use(MarkdownItAbbr)
+  .use(MarkdownItAnchor)
+  .use(MarkdownItFootnote)
+  .use(MarkdownItHighlightjs)
+  .use(MarkdownItSub)
+  .use(MarkdownItSup)
+  .use(MarkdownItTasklists)
+  .use(MarkdownItTOC);
+
+
+defineProps<{
+    content: string
+}>()
+</script>
+
+<template>
+    <div v-html="markdown.render(content)" />
+</template>
