1fc43501c6e3dde886b020fc12e6680e7c929285
Roberth González <reliutg@gmail.com>
2024-05-04 18:22:52 +0200

    chore: update app title in head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  app: {
+    head: {
+      title: "Nuxt + LlamaIndex",
+    }
+  },
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "nuxt-icon", '@vueuse/nuxt', 'radix-vue/nuxt']
 })
