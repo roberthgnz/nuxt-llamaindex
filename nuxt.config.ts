0216b3e77005a0a0e6b805b9e6c9f2fd0ea6b0fb
Roberth González <reliutg@gmail.com>
2024-05-04 17:40:03 +0200

    chore: add radix-vue/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "nuxt-icon", '@vueuse/nuxt']
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "nuxt-icon", '@vueuse/nuxt', 'radix-vue/nuxt']
 })
